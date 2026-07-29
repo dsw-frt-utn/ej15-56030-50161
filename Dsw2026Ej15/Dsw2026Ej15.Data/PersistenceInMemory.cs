0090808e89dc75e5d64fb03711cdc483fa8f6377
Maria Elizabeth Morales <MariaElizabeth.Morales@alu.frt.utn.edu.ar>
2026-07-29 17:35:57 -0300

    Verificacion de errores

--- Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceInMemory.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceInMemory.cs
@@ -19,21 +19,20 @@ namespace Dsw2026Ej15.Data
             LoadSpecialities();
         }
 
-        public void LoadSpecialities()
+        private void LoadSpecialities()
         {
             try
             {
                 string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sources", "specialities.json");
-
                 var json = File.ReadAllText(jsonPath);
-                var specialities = JsonSerializer.Deserialize<List<SpecialityDto>>
-                    (json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true }) ?? [];
+                var specialities = JsonSerializer.Deserialize<List<SpecialityDto>>(
+                    json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ?? [];
                 _specialities = [.. specialities.Select(s => new Speciality(s.Name, s.Description, s.Id))];
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                Console.Error.WriteLine($"Error cargando specialities.json: {ex.Message}");
             }
-
         }
 
         public Speciality? GetSpecialityById(Guid id)
